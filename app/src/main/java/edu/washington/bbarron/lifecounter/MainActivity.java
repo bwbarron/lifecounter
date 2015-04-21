429ddfaa3b6cee89257c7e8186068556e04ddb99
bwbarron <brandon.barron@live.com>
2015-04-21 01:17:09 -0700

    Created framework for one player
    created a linear layout and buttons for one player

---
+++ app/src/main/java/edu/washington/bbarron/lifecounter/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.washington.bbarron.lifecounter;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Button b1;
+    private Button b2;
+    private Button b3;
+    private Button b4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+    }
+
+    public void onClick(View view) {
+
+    }
+}
